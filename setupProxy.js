f55f6b43fc68bf66689ab95ff9a84b9f3d4af99a
chanwoosung <chanwoosung@likelion.net>
2022-10-26 20:51:30 +0900

    test

--- setupProxy.js
+++ setupProxy.js
@@ -1,11 +1,11 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-export default function (app) {
+module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://auth-proxy.goerli-alpha.croffle.me',
+      target: 'http://localhost:5000',
       changeOrigin: true,
     })
   );
-}
+};
